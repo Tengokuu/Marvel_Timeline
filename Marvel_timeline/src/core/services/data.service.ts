747ccffcf333b4f2af3e19b868f2f6a3f7693c55
Tnegoku <mati.jasinski2008@gmail.com>
2025-12-19 11:31:41 +0100

    Add files via upload

---
+++ Marvel_timeline/src/core/services/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MCUItem } from '../../app/models/mcu-item.model';
+
+@Injectable({ providedIn: 'root' })
+export class DataService {
+  private dataUrl = 'Assets/data/mcu_movies_data.json';
+
+  constructor(private http: HttpClient) {}
+
+  getTimeline(): Observable<MCUItem[]> {
+    return this.http.get<MCUItem[]>(this.dataUrl);
+  }
+}
