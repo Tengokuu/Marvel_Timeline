1bd4f4d025cd86aa3019753e719d8618be1a2fa5
Tengokuu <mati.jasinski2008@gmail.com>
2025-12-31 11:29:00 +0100

    nowe zmiany trzeba przetestować.nop

--- Marvel_timeline/src/app/app.ts
+++ Marvel_timeline/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// Ścieżka do modelu: wchodzimy do folderu models (jest w tym samym folderze co app.ts)
+import { MCUItem } from './models/mcu-item.model';
+// Ścieżka do serwisu: musimy wyjść jeden poziom wyżej (..), żeby wejść do core
+import { DataService } from '../core/services/data.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('Marvel_timeline');
+export class App implements OnInit {
+  mcuItems: MCUItem[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getTimeline().subscribe({
+      next: (data) => {
+        this.mcuItems = data;
+        console.log('Dane załadowane:', this.mcuItems);
+      },
+      error: (err) => console.error('Błąd ładowania danych:', err)
+    });
+  }
 }
